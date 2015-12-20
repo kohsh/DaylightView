2194f1b64ccb3d30618c65303a0309afc09a70f4
Kyle O'Shaughnessy <kyle.oshaughnessy@gmail.com>
2015-12-19 23:40:01 -0700

    Initial Commit

---
+++ DaylightView/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DaylightView
+//
+//  Created by Kyle on 2015-12-19.
+//  Copyright (c) 2015 Kyle O'Shaughnessy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DaylightView/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DaylightView
+//
+//  Created by Kyle on 2015-12-19.
+//  Copyright (c) 2015 Kyle O'Shaughnessy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
